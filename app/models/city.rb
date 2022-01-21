553d1dc987e2f81f02ce7d257b209bef03047088
Clay Stewart <c.stewart89@gmail.com>
2022-01-21 01:37:46 -0500

    Solution for lab final test in listing_spec may be broken.

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,12 +3,7 @@ class City < ActiveRecord::Base
   has_many :listings, through: :neighborhoods
   has_many :reservations, through: :listings
 
-<<<<<<< HEAD
-  def city_openings(start_date, end_date)
-    
-    res.map { |r| r.listing }
-=======
   def city_openings
->>>>>>> 3a8f01b6e1532e51df9fbb8ea6e0fdfab6b7ba7b
+
   end
 end
